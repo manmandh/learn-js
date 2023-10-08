bab01760d2207db05a8a1c75f55359a32dce6923
manmandh <mantm.22ite@vku.udn.vn>
2023-10-08 16:41:31 +0700

    #1 data type, null undefine NaN

---
+++ type.js
@@ -0,0 +1,24 @@
+let name = 'man'; //tự ép kiểu name là string
+let numOfStudent = 4; //tự nhận dạng numOfStudent là number
+let studentObject = {
+  name: 'man',
+  age: '19',
+};
+
+let x = new Number(10);
+let s = new String('Hello');
+let z = new Boolean(true);
+
+let obj = new Object();
+
+let obj2 = {};
+
+let birthDay1 = new Date(); //lấy thời gian hiện tại
+
+// result
+// Wed Apr 26 2023 21:38:43 GMT+0700 (Indochina Time)
+
+let birthDay2 = new Date(2000, 3, 23, 5, 6, 9, 34); //đặt thời gian là ngày ngày 23/3/2000 05:06:09:34
+
+// result
+// Sun Apr 23 2000 05:06:09 GMT+0700 (Indochina Time) {}
