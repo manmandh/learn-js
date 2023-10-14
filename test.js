78e0f50207e2cb40a20f1055a876ccd3a4b51b1d
manmandh <mantm.22ite@vku.udn.vn>
2023-10-14 20:51:05 +0700

    #4 Add: robot #5

---
+++ test.js
@@ -0,0 +1,9 @@
+//Control flow
+let temperature = 25;
+if (temperature > 30) {
+  console.log("It's hot outside!"); //not be executed
+} else if (temperature > 20) {
+  console.log("It's warm outside!"); // be executed
+} else {
+  console.log("It's cool outside!"); // not be executed
+}
